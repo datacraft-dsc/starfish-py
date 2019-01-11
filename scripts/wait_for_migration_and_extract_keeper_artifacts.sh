eafd737bf8351ee86f4a2df867bf75fea713bf89
Bill Barman <bill@anantya.com>
2019-01-11 10:03:01 +0800

    debug why travis is not running

--- scripts/wait_for_migration_and_extract_keeper_artifacts.sh
+++ scripts/wait_for_migration_and_extract_keeper_artifacts.sh
@@ -15,8 +15,10 @@ mkdir -p ${CONTRACT_FOLDER}
 
 until [ ${CONTRACTS_READY} -eq 0 ] || [ ${RETRY_COUNT} -eq 120 ]; do
     KEEPER_CONTRACTS_DOCKER_ID=$(docker container ls | grep keeper-contracts | awk '{print $1}')
-    docker cp ${KEEPER_CONTRACTS_DOCKER_ID}:/keeper-contracts/artifacts/ready ./${CONTRACT_FOLDER}/
-    if [[ -f "${CONTRACT_FOLDER}/ready" ]]; then
+    docker cp ${KEEPER_CONTRACTS_DOCKER_ID}:/keeper-contracts/artifacts/. ./${CONTRACT_FOLDER}/
+    CONTRACT_FILES=(${CONTRACT_FOLDER}/*.json)
+    # test for the ready file and a json file
+    if [ -f "${CONTRACT_FILES}" ] && [ -f "${CONTRACT_FOLDER}/ready" ]; then    
         CONTRACTS_READY=0
         break
     fi
@@ -29,6 +31,6 @@ if [ ! -f ${CONTRACT_FOLDER}/ready ]; then
     exit 1
 fi
 
-docker cp ${KEEPER_CONTRACTS_DOCKER_ID}:/keeper-contracts/artifacts/. ./${CONTRACT_FOLDER}/
+# docker cp ${KEEPER_CONTRACTS_DOCKER_ID}:/keeper-contracts/artifacts/. ./${CONTRACT_FOLDER}/
 echo "copied over the following contracts"
 ls -lrth ${CONTRACT_FOLDER}/*
