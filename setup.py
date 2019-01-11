b52ad2219a1e0e625df91c92ad39e96d189c1104
Bill Barman <bill@anantya.com>
2019-01-11 10:55:02 +0800

    change ver no

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('CHANGELOG.md') as changelog_file:
     changelog = changelog_file.read()
 
 install_requirements = [
-    'squid-py==0.2.17',
+    'squid-py==0.2.19',
     'coloredlogs',
     'eciespy',
     'pyopenssl',
