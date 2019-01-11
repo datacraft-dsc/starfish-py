3cfcd4f3b2e86fb5db6a49802f866278b89ceab7
Bill Barman <bill@anantya.com>
2019-01-11 09:41:06 +0800

    debug why travis is not running

--- scripts/wait_for_migration_and_extract_keeper_artifacts.sh
+++ scripts/wait_for_migration_and_extract_keeper_artifacts.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
 RETRY_COUNT=0
-COMMAND_STATUS=1
+CONTRACTS_READY=1
 
-mkdir -p artifacts
+CONTRACT_FOLDER=artifacts
 
-until [ $COMMAND_STATUS -eq 0 ] || [ $RETRY_COUNT -eq 120 ]; do
+# make sure that we copy over the latest contrats, so remove any old folder
+if [ -d ${CONTRACT_FOLDER} ]; then
+    rm -rf ${CONTRACT_FOLDER}
+fi
+
+# receate empty
+mkdir -p ${CONTRACT_FOLDER}
+
+until [ ${CONTRACTS_READY} -eq 0 ] || [ ${RETRY_COUNT} -eq 120 ]; do
     KEEPER_CONTRACTS_DOCKER_ID=$(docker container ls | grep keeper-contracts | awk '{print $1}')
-    docker cp ${KEEPER_CONTRACTS_DOCKER_ID}:/keeper-contracts/artifacts/ready ./artifacts/
-    COMMAND_STATUS=$?
+    docker cp ${KEEPER_CONTRACTS_DOCKER_ID}:/keeper-contracts/artifacts/ready ./${CONTRACT_FOLDER}/
+    if [[ -f "${CONTRACT_FOLDER}/ready" ]]; then
+        CONTRACTS_READY=0
+        break
+    fi
     sleep 5
     let RETRY_COUNT=RETRY_COUNT+1
 done
 
-if [ $COMMAND_STATUS -ne 0 ]; then
+if [ ! -f ${CONTRACT_FOLDER}/ready ]; then
     echo "Waited for more than ten minutes, but keeper contracts have not been migrated yet. Did you run an Ethereum RPC client and the migration script?"
     exit 1
 fi
 
-docker cp ${KEEPER_CONTRACTS_DOCKER_ID}:/keeper-contracts/artifacts/. ./artifacts/
-echo "copied over contracts"
-ls -lrth ./artifacts
+docker cp ${KEEPER_CONTRACTS_DOCKER_ID}:/keeper-contracts/artifacts/. ./${CONTRACT_FOLDER}/
+echo "copied over the following contracts"
+ls -lrth ${CONTRACT_FOLDER}/*
