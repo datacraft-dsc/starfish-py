5374ee1ae3e169309d7a7418d0cab5944d678549
billbsing <bill.barman@dex.sg>
2019-01-26 08:01:14 +0800

    fix comments and cleanup

--- ocean_py/agent/purchase_agent.py
+++ ocean_py/agent/purchase_agent.py
@@ -10,15 +10,17 @@ from ocean_py.agent.agent_base import AgentBase
 
 class PurchaseAgent(AgentBase):
     def __init__(self, ocean):
-        """init a standard ocean agent, with a given DID"""
+        """init a standard ocean agent"""
         AgentBase.__init__(self, ocean)
 
 
     def purchase_asset(self, asset, account):
         """
-        Register an asset with the agent storage server
+        Purchase an asset with the agent storage server
         :param asset: asset to purchase
-        :param account: account to use for buying this asset
+        :param account: account unlocked and has sufficient funds to buy this asset
+        
+        :return: service_agreement_id of the purchase or None if no purchase could be made
         """
         service_agreement_id = None
         service_agreement = self.get_service_agreement_from_asset(asset)
@@ -28,12 +30,20 @@ class PurchaseAgent(AgentBase):
         return service_agreement_id
 
     def consume_asset(self, asset, service_agreement_id, account):
+        """
+        Conusmer the asset data, by completing the payment and later returning the data for the asset
+        
+        """
         downloads_path = self._ocean.squid._downloads_path
         service_agreement = self.get_service_agreement_from_asset(asset)
         if service_agreement:
             self._ocean.squid.consume_service(service_agreement_id, asset.did, service_agreement.sa_definition_id, account)
 
     def is_access_granted_for_asset(self, asset, service_agreement_id, account):
+        """
+        Return true if we have access to the asset's data using the service_agreement_id and account used 
+        to purchase this asset
+        """
         account_address = None
         if isinstance(account, object):
             account_address = account.address
@@ -49,6 +59,9 @@ class PurchaseAgent(AgentBase):
         return self._ocean.squid.is_access_granted(service_agreement_id, asset.did, account_address)
 
     def get_service_agreement_from_asset(self, asset):
+        """
+        return the service agreement definition for this asset
+        """
         service_agreement = None
         ddo = asset.ddo
 
@@ -60,6 +73,9 @@ class PurchaseAgent(AgentBase):
 
     @staticmethod
     def wait_for_event(event, arg_filter, wait_iterations=20):
+        """
+        Method used to wait for the service agreements to complete
+        """
         _filter = event.createFilter(fromBlock=0, argument_filters=arg_filter)
         for check in range(wait_iterations):
             events = _filter.get_all_entries()
