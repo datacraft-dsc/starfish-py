7fef4037ae22c418273b463d35d6923e0c92410d
billbsing <billbsing.gmail.com>
2019-01-28 16:46:29 +0800

    put back to original number of accounts

--- tests/test_ocean_cli.py
+++ tests/test_ocean_cli.py
@@ -47,7 +47,7 @@ def test_ocean_cli():
         assert header == lines[1]
         for i in range(num_accounts):
             account = lines[i + 2].split()
-            assert 4 == len(account)
+            assert 3 == len(account)
         last_account_id = account[1]
         # FUTURE assert valid_id(last_account_id)
         assert 0 == len(lines[num_accounts + 2])
@@ -64,7 +64,7 @@ def test_ocean_cli():
         assert header == lines[1]
         for i in range(num_accounts):
             account = lines[i + 2].split()
-            assert 4 == len(account)
+            assert 3 == len(account)
         last_account_id2 = account[1]
         # FUTURE assert valid_id(last_account_id2)
         assert 0 == len(lines[num_accounts + 2])
