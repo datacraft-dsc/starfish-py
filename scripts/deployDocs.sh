1841a931eff4b0020645ccedfbc63efda6605fed
billbsing <bill.barman@dex.sg>
2019-02-02 10:59:25 +0800

    cleanup, and add skip cleanup in travis

--- scripts/deployDocs.sh
+++ scripts/deployDocs.sh
@@ -2,12 +2,15 @@
 
 # package docs and deploy
 
-make docs
 DOC_PATH='./docs'
 VERSION=`cat $DOC_PATH/source/conf.py | egrep 'release\s?=' | awk '{print $3}' | sed -e "s/^'//" -e "s/'$//"`
 PROJECT_NAME=`cat $DOC_PATH/source/conf.py | egrep 'project\s?=' | awk '{print $3}' | sed -e "s/^'//" -e "s/'$//"`
 SOURCE_FILES="$DOC_PATH/build/html/"
 PACKAGE_NAME="docs_${PROJECT_NAME}_${VERSION}"
 
-echo "building package $PACKAGE_NAME"
+echo "building docs package $PACKAGE_NAME"
+# make the docs from source
+make docs
+
+# package into a tar.gz file for deployment
 tar -czvf "${DOC_PATH}/${PACKAGE_NAME}.tar.gz" "$SOURCE_FILES"
