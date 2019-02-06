383c7855c5d377bf8b24fe3db6b01b6520833b37
billbsing <bill.barman@dex.sg>
2019-02-06 16:16:34 +0800

    add build url after deploy

--- scripts/deployDocs.sh
+++ scripts/deployDocs.sh
@@ -7,6 +7,7 @@ VERSION=`cat $DOC_PATH/source/conf.py | egrep 'release\s?=' | awk '{print $3}' |
 PROJECT_NAME=`cat $DOC_PATH/source/conf.py | egrep 'project\s?=' | awk '{print $3}' | sed -e "s/^'//" -e "s/'$//"`
 SOURCE_FILES="$DOC_PATH/build/html/"
 PACKAGE_NAME="docs_${PROJECT_NAME}_${VERSION}"
+DEPLOY_FILENAME="${DOC_PATH}/${PACKAGE_NAME}.tar.gz"
 
 DEPLOY_USER=docs_deploy
 
@@ -18,7 +19,6 @@ if [ ! -z "$2" ]; then
     DEPLOY_USER="$2"
 fi
 
-
 echo "building docs package $PACKAGE_NAME"
 
 # install dev packages for doc build
@@ -28,15 +28,19 @@ pip install -e .[dev] -U tox-travis
 make docs
 
 # package into a tar.gz file for deployment
-tar -czvf "${DOC_PATH}/${PACKAGE_NAME}.tar.gz" "$SOURCE_FILES"
+tar -czvf "$DEPLOY_FILENAME" "$SOURCE_FILES"
 
 if [ ! -z "$DEPLOY_SERVER" ]; then
+    DEPLOY_BUILD_URL="http://${DEPLOY_SERVER}/docs_build"
+
     echo "Deploying doc file to $DEPLOY_SERVER"
     openssl aes-256-cbc -K $encrypted_e14c59e0af38_key -iv $encrypted_e14c59e0af38_iv \
     -in docs/keys/dex-docs-deploy.enc \
     -out /tmp/dex-docs-deploy -d
 
     chmod 0600 /tmp/dex-docs-deploy
-    scp -i /tmp/dex-docs-deploy "${DOC_PATH}/${PACKAGE_NAME}.tar.gz" ${DEPLOY_USER}@${DEPLOY_SERVER}:
+    scp -i /tmp/dex-docs-deploy "$DEPLOY_FILENAME" ${DEPLOY_USER}@${DEPLOY_SERVER}:
     rm /tmp/dex-docs-deploy
+
+    curl -H "Content-Type: application/json" -X POST -d '{"file":"$DEPLOY_FILENAME"}' "$DEPLOY_BUILD_URL"
 fi
