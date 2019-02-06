06b62944035cca1a89fdb5ee3d4a4beadaa8cc73
billbsing <bill.barman@dex.sg>
2019-02-06 14:19:27 +0800

    add enc key and travis decryption

--- scripts/deployDocs.sh
+++ scripts/deployDocs.sh
@@ -8,6 +8,7 @@ PROJECT_NAME=`cat $DOC_PATH/source/conf.py | egrep 'project\s?=' | awk '{print $
 SOURCE_FILES="$DOC_PATH/build/html/"
 PACKAGE_NAME="docs_${PROJECT_NAME}_${VERSION}"
 
+
 echo "building docs package $PACKAGE_NAME"
 
 # install dev packages for doc build
@@ -18,3 +19,5 @@ make docs
 
 # package into a tar.gz file for deployment
 tar -czvf "${DOC_PATH}/${PACKAGE_NAME}.tar.gz" "$SOURCE_FILES"
+
+scp -i /tmp/dex-deploy-docs "${DOC_PATH}/${PACKAGE_NAME}.tar.gz" docs_deploy@shrimp.octet.services
