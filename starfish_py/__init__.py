4f4ee6920c07b24971f7e15508b762768282fd73
billbsing <billbsing.gmail.com>
2019-02-11 15:36:43 +0800

    codacy fix

--- starfish_py/__init__.py
+++ starfish_py/__init__.py
@@ -2,15 +2,13 @@
     Starfish-py module
 
     This module contains the following classes
-    
+
     * :class:`Agent` The Agent wrapper class.
     * :class:`Asset` The Asset class.
     * :class:`Config` The Config data class.
     * :class:`Ocean` The main Ocean connection class.
     * :data:`logger` The logger class.
-
 """
-
 
 __author__ = """DEX.sg"""
 __version__ = '0.0.1'
@@ -18,7 +16,6 @@ __version__ = '0.0.1'
 import logging
 
 logger = logging.getLogger(__name__)
-
 
 from .agent import Agent
 from .asset.asset import Asset
