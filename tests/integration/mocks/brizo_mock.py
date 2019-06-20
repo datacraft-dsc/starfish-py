639ba48d5efc08741dc7a38e92540b8dbc66c412
billbsing <bill.barman@dex.sg>
2019-06-20 08:48:24 +0800

    put back serperate event listner in BrizoMock

--- tests/integration/mocks/brizo_mock.py
+++ tests/integration/mocks/brizo_mock.py
@@ -9,12 +9,12 @@ from eth_utils import add_0x_prefix
 from squid_py import ConfigProvider
 from squid_py.brizo.brizo import Brizo
 from squid_py.did import id_to_did, did_to_id
-# from squid_py.agreements.register_service_agreement import register_service_agreement_publisher
 from squid_py.agreements.service_agreement import ServiceAgreement
 from squid_py.agreements.service_types import ServiceTypes
 from squid_py.keeper.web3_provider import Web3Provider
 from squid_py.keeper import Keeper
 from squid_py.keeper.events_manager import EventsManager
+from squid_py.agreements.manager import AgreementsManager
 
 from starfish.logging import setup_logging
 
@@ -26,19 +26,86 @@ class BrizoMock(object):
 
     def __init__(self, ocean_instance=None, account=None):
         self._ocean_instance = ocean_instance
+        self._is_event_subscribed = False
+        self._ddo_records = {}
 
     def subscribe(self, ocean, account, did, ddo):
         self._account = account
         model = ocean.get_squid_model()
         self._ocean_instance = model.get_squid_ocean(account)
+        self._is_event_subscribed = False
 
+        self._ddo_records[did] = ddo
         events_manager = EventsManager.get_instance(Keeper.get_instance())
         events_manager.stop_all_listeners()
-
-        self._ocean_instance.agreements.watch_provider_events(self._account)
+        time.sleep(1)
+        events_manager.agreement_listener._event_filters = dict()
+        self._ocean_instance.agreements.subscribe_events(
+            self._account.address,
+            self._handle_agreement_created,
+        )
+        # at the moment we need to do this sleep or the event handle below
+        # is not called. Not sure why?
         time.sleep(1)
 
+    def _handle_agreement_created(self, event, *_):
+#        print('_handle_agreement_created ', event)
+        try:
+            if not event or not event.args:
+                print('no handle created')
+                return
 
+            self._is_event_subscribed = True
+            print(f'Start handle_agreement_created: event_args={event.args}')
+            config = ConfigProvider.get_config()
+            provider_account = self._account
+            assert provider_account.address == event.args['_accessProvider']
+
+            did = id_to_did(event.args['_did'])
+            agreement_id = Web3Provider.get_web3().toHex(event.args['_agreementId'])
+
+            ddo = self._ddo_records[did]
+            # calling the resolve calls the squid_py.aquarius module.
+            # The module calls a http request session object created outside of this thread
+            # and so can cause crashes
+            #ddo = ocean.assets.resolve(did)
+            sa = ServiceAgreement.from_ddo(ServiceTypes.ASSET_ACCESS, ddo)
+
+            condition_ids = sa.generate_agreement_condition_ids(
+                agreement_id=agreement_id,
+                asset_id=add_0x_prefix(did_to_id(did)),
+                consumer_address=event.args['_accessConsumer'],
+                publisher_address=ddo.publisher,
+                keeper=Keeper.get_instance())
+
+            events_manager = EventsManager.get_instance(Keeper.get_instance())
+            agreements_manager = AgreementsManager(
+                config,
+                Keeper.get_instance(),
+                events_manager,
+                config.storage_path
+            )
+            agreements_manager.register_publisher(
+                event.args['_accessConsumer'],
+                agreement_id,
+                did,
+                sa,
+                sa.service_definition_id,
+                sa.get_price(),
+                provider_account,
+                condition_ids
+            )
+            print(f'handle_agreement_created() -- done registering event listeners.')
+        except e as Exception:
+            message = f'error with exception {e}'
+            print(message)
+            logging.error(message)
+            raise
+            
+    @property
+    def is_event_subscribed(self):
+        return self._is_event_subscribed
+            
     def initialize_service_agreement(did, agreement_id, service_definition_id,
                                      signature, account_address, purchase_endpoint):
         print(f'BrizoMock.initialize_service_agreement: purchase_endpoint={purchase_endpoint}')
