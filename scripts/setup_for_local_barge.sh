9b39e4bfe8aea6b2f4b41170bfafe8ab6f3ad69d
billbsing <bill.barman@dex.sg>
2019-06-27 10:24:35 +0800

    redo startup sequence in travis

--- scripts/setup_for_local_barge.sh
+++ scripts/setup_for_local_barge.sh
@@ -6,7 +6,9 @@ until [ $RETRY -gt 5 ]; do
     git checkout dex-2019-06-17
     ./start_ocean.sh --no-brizo --no-pleuston --local-spree-node 2>&1 > barge.log &
     cd ..
-    sleep 120
+    sleep 240
+    echo "Waiting for keeper contracts"
+    ./scripts/wait_for_migration_and_extract_keeper_artifacts.sh
     SURFER_FAIL=`grep 'ocean_surfer.*exited' barge/barge.log`
     if [ ! -z $SURFER_FAIL ]; then
         echo "surfer failed to startup"
@@ -17,5 +19,4 @@ until [ $RETRY -gt 5 ]; do
         RETRY=6
     fi
 done
-./scripts/wait_for_migration_and_extract_keeper_artifacts.sh
 ./scripts/wait_for_surfer.sh http://localhost:8080
