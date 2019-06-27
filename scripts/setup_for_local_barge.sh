f054c5d9af07ff03a28c93d6f01badb5353c3dc5
billbsing <bill.barman@dex.sg>
2019-06-27 10:17:59 +0800

    add surfer retry to start barge

--- scripts/setup_for_local_barge.sh
+++ scripts/setup_for_local_barge.sh
@@ -1,9 +1,21 @@
 git clone https://github.com/DEX-Company/barge.git
-cd barge
-git checkout dex-2019-06-17
-# bash -x start_ocean.sh --no-brizo --no-pleuston --local-spree-node 2>&1 > start_ocean.log &
-./start_ocean.sh --no-brizo --no-pleuston --local-spree-node &
-cd ..
-sleep 240
+
+RETRY=0
+until [ $RETRY -gt 5 ]; do
+    cd barge
+    git checkout dex-2019-06-17
+    ./start_ocean.sh --no-brizo --no-pleuston --local-spree-node 2>&1 > barge.log &
+    cd ..
+    sleep 120
+    SURFER_FAIL=`grep 'ocean_surfer.*exited' barge/barge.log`
+    if [ ! -z SURFER_FAIL ]; then
+        echo "surfer failed to startup"
+        echo "Stoping all docker containers"
+        docker kill $(docker ps -a -q)
+        ((RETRY++))
+    else
+        RETRY=6
+    fi
+done
 ./scripts/wait_for_migration_and_extract_keeper_artifacts.sh
 ./scripts/wait_for_surfer.sh http://localhost:8080
