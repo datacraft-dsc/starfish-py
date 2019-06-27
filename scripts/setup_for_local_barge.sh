d540301bae6fdce7847e67c7d0a936a46a7ad155
billbsing <bill.barman@dex.sg>
2019-06-27 17:15:59 +0800

    better barge restart

--- scripts/setup_for_local_barge.sh
+++ scripts/setup_for_local_barge.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/DEX-Company/barge.git
 
-RETRY=0
+DOCKER_RETRY=1
 BARGE_DELAY=240
-until [ $RETRY -gt 5 ]; do
+until [ $DOCKER_RETRY -gt 10 ]; do
     cd barge
     git checkout dex-2019-06-17
     ./start_ocean.sh --no-brizo --no-pleuston --local-spree-node 2>&1 > barge.log &
@@ -13,13 +13,14 @@ until [ $RETRY -gt 5 ]; do
     ./scripts/wait_for_migration_and_extract_keeper_artifacts.sh
     SURFER_FAIL=`egrep 'ocean_surfer.*exited' barge/barge.log`
     if [ -z "$SURFER_FAIL" ]; then
-        RETRY=10
+        DOCKER_RETRY=100
         echo "surfer is running"
     else
-        echo "surfer failed to startup"
+        echo "surfer failed to startup retry No. ${DOCKER_RETRY}"
         echo "Stopping all docker containers"
         docker kill $(docker ps -a -q)
-        ((RETRY++))
+        sudo systemctl restart docker
+        ((DOCKER_RETRY++))
         BARGE_DELAY=60
     fi
     echo "------------------------ Start Barge Log -----------------------"
