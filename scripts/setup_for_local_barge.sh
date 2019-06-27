eabc3ad82dc76e9fd467ca9fdcc0f59de82bbca0
billbsing <bill.barman@dex.sg>
2019-06-27 10:59:20 +0800

    barge startup script

--- scripts/setup_for_local_barge.sh
+++ scripts/setup_for_local_barge.sh
@@ -1,12 +1,14 @@
 git clone https://github.com/DEX-Company/barge.git
 
 RETRY=0
+BARGE_DELAY=240
 until [ $RETRY -gt 5 ]; do
     cd barge
     git checkout dex-2019-06-17
     ./start_ocean.sh --no-brizo --no-pleuston --local-spree-node 2>&1 > barge.log &
     cd ..
-    sleep 120
+    echo "sleeping for $BARGE_DELAY seconds"
+    sleep $BARGE_DELAY
     echo "Waiting for keeper contracts to be build"
     ./scripts/wait_for_migration_and_extract_keeper_artifacts.sh
     SURFER_FAIL=`egrep 'ocean_surfer.*exited' barge/barge.log`
@@ -16,11 +18,13 @@ until [ $RETRY -gt 5 ]; do
         echo "surfer is running"
     else
         echo "surfer failed to startup"
-        echo "Stoping all docker containers"
+        echo "Stopping all docker containers"
         docker kill $(docker ps -a -q)
         ((RETRY++))
+        BARGE_DELAY=60
     fi
-    echo "------------------------ Barge Log ----------------------"
+    echo "------------------------ Start Barge Log -----------------------"
     cat barge/barge.log
+    echo "------------------------ End Barge Log -------------------------"
 done
 # ./scripts/wait_for_surfer.sh http://localhost:8080
