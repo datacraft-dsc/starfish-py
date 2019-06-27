83e604553c4ed7ed6c0ea6a4f906d07b016177b3
billbsing <bill.barman@dex.sg>
2019-06-27 09:42:14 +0800

    fix start barge

--- scripts/setup_for_local_barge.sh
+++ scripts/setup_for_local_barge.sh
@@ -2,7 +2,7 @@ git clone https://github.com/DEX-Company/barge.git
 cd barge
 git checkout dex-2019-06-17
 # bash -x start_ocean.sh --no-brizo --no-pleuston --local-spree-node 2>&1 > start_ocean.log &
-start_ocean.sh --no-brizo --no-pleuston --local-spree-node &
+./start_ocean.sh --no-brizo --no-pleuston --local-spree-node &
 cd ..
 sleep 480
 scripts/wait_for_migration_and_extract_keeper_artifacts.sh
