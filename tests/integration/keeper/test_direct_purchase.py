c2d26758d18e51a48da940dc19a440fc7ee2e6ef
Ilia <ilyabukalov@gmail.com>
2019-08-29 18:17:20 +0800

    Direct purchase integration test. payment done

--- tests/integration/keeper/test_direct_purchase.py
+++ tests/integration/keeper/test_direct_purchase.py
@@ -11,6 +11,14 @@ def test_direct_purchase(ocean, config, squid_agent):
     account_publisher = ocean.get_account(config.publisher_account['address'])
     account_purchaser.set_password(config.purchaser_account['password'])
     account_purchaser.unlock()
-    account_purchaser.request_tokens(50)
+    TOKEN_AMOUNT_TO_TRANSFER = 100
+    account_purchaser.request_tokens(TOKEN_AMOUNT_TO_TRANSFER)
 
-    adapter.approve_tokens(purchase_contract.address, 50, account_purchaser)
+    balance_purchaser = account_purchaser.ocean_balance_raw
+    balance_publisher = account_publisher.ocean_balance_raw
+
+    adapter.approve_tokens(purchase_contract.address, TOKEN_AMOUNT_TO_TRANSFER, account_purchaser)
+    account_purchaser.unlock()
+    purchase_contract.send_token_and_log(account_purchaser, account_publisher.address, TOKEN_AMOUNT_TO_TRANSFER, 3, 4)
+
+    assert(account_purchaser.ocean_balance_raw + TOKEN_AMOUNT_TO_TRANSFER == balance_purchaser and account_publisher.ocean_balance_raw - TOKEN_AMOUNT_TO_TRANSFER == balance_publisher)
