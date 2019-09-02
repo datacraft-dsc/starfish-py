8bc81bf9162a42fe5f70f7a929b3e8a756c2273d
billbsing <bill.barman@dex.sg>
2019-09-02 11:51:27 +0800

    cleanup comments, add barge branch for direct-purchase for testing

--- starfish/keeper/direct_purchase.py
+++ starfish/keeper/direct_purchase.py
@@ -10,9 +10,10 @@ class DirectPurchase(ContractBase):
         """
         Send tokens to address with tracking record in log.
 
-        :param Account Account address and passowrd of the send from Account
+        :param account: The Account to send the tokens from
+        :type account: :class:`.Account` object
         :param address_to: Account address, str
-        :param amount: token amount, int
+        :param amount: token amount, int in unit of Ocean vodka's
         :param reference1: reference in log, int256
         :param reference2: reference in log, int256
         :return: void
@@ -22,14 +23,15 @@ class DirectPurchase(ContractBase):
             address_to = Web3Provider.get_web3().toChecksumAddress(address_to)
 
         tx_hash = self.send_transaction(
-            'sendTokenAndLog',
-            (address_to,
-             amount,
-             Web3.toBytes(reference1),
-             Web3.toBytes(reference2)
-             ),
-            transact={'from': account.address,
-                      'passphrase': account.password,
+            'sendTokenAndLog', (
+                address_to,
+                amount,
+                Web3.toBytes(reference1),
+                Web3.toBytes(reference2)
+            ),
+            transact={
+                'from': account.address,
+                'passphrase': account.password,
             }
         )
         return self.get_tx_receipt(tx_hash).status == 1
