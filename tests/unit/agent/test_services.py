dba714bd831673554f0d42d00e99cd2ed2761019
billbsing <bill.barman@dex.sg>
2020-02-06 15:56:53 +0800

    cleanup test

--- tests/unit/agent/test_services.py
+++ tests/unit/agent/test_services.py
@@ -9,7 +9,7 @@ from starfish.agent.services import (
 )
 
 
-def test_init():
+def test_add_single_default():
     # I want to add a metadata service using the default url & version
     services = Services('http://localhost')
     services.add('metadata')
@@ -19,6 +19,7 @@ def test_init():
     assert(result['metadata']['url'] == 'http://localhost/api/v1/meta/data')
 
 
+def test_add_single_version_default():
     # I want to add metadata service using the a default version
     services = Services('http://localhost', version='v99')
     services.add('metadata')
@@ -29,6 +30,7 @@ def test_init():
     assert('metadata' in services.names)
 
 
+def test_add_multiple():
     # I want to add only metadata and storage services
     services = Services('http://localhost', service_list=['metadata', 'storage'])
     result = services.as_dict
@@ -39,16 +41,21 @@ def test_init():
     assert(result['storage']['url'] == 'http://localhost/api/v1/assets')
 
 
+def test_add_all():
     # I want to add all default services
     services = Services('http://localhost', all_services=True)
     result = services.as_dict
     assert(result)
     assert(services.names == ALL_SERVICES)
 
+def test_add_unknown_default():
     # I want to see when a add an unkown service name using the default serivce type
+    services = Services('http://localhost', all_services=True)
+    result = services.as_dict
     with pytest.raises(ValueError):
         services.add('bad-name')
 
+def test_add_new_service():
     # I want to add a new service uri and service type
     services = Services('http://localhost', all_services=True)
     services.add('test', 'api/v1/test', 'NEW.v1.test')
