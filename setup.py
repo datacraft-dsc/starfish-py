70594e797992d1aa4d2f70534c7f24620887b891
billbsing <bill.barman@dex.sg>
2020-02-27 13:59:05 +0800

    Bump version: 0.6.5 → 0.6.6

--- setup.py
+++ setup.py
@@ -97,6 +97,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/DEX-Company/starfish-py',
-    version='0.6.5',
+    version='0.6.6',
     zip_safe=False,
 )
