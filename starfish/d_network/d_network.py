67748dfb7038a95ff818ea4490af23e7473d044e
billbsing <bill.barman@dex.sg>
2020-03-25 10:29:17 +0800

    fix lint

--- starfish/d_network/d_network.py
+++ starfish/d_network/d_network.py
@@ -30,10 +30,9 @@ NETWORK_NAMES = {
     77: 'POA_Sokol',
     99: 'POA_Core',
     100: 'xDai',
-    8995: 'nile',           # Ocean Protocol Public test net
-    8996: 'spree'           # Ocean Protocol local test net
-    0xcea11: 'pacific'      # Ocean Protocol Public mainnet
-
+    8995: 'nile',                   # Ocean Protocol Public test net
+    8996: 'spree',                  # Ocean Protocol local test net
+    0xcea11: 'pacific'              # Ocean Protocol Public mainnet
 }
 
 CONTRACT_LIST = {
