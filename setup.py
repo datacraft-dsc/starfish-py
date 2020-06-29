0f9ac01d188efdc6ddc054f0b333caafd81df87b
billbsing <bill.barman@dex.sg>
2020-06-29 15:52:16 +0800

    Bump version: 0.10.0 → 0.11.0

--- setup.py
+++ setup.py
@@ -81,6 +81,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/DEX-Company/starfish-py',
-    version='0.10.0',
+    version='0.11.0',
     zip_safe=False,
 )
--- starfish/__init__.py
+++ starfish/__init__.py
@@ -7,4 +7,4 @@
 from starfish.network import Network                     # noqa: F401
 
 __author__ = """DEX.sg"""
-__version__ = '0.10.0'
+__version__ = '0.11.0'
