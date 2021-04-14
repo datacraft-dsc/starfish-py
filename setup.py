5c154df90ab930bc000b63d4754255c33b797b7f
bill <billbsing@gmail.com>
2021-04-14 10:51:28 +0800

    upgrade convex-api

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requirements = [
     'web3',
     'typing_extensions',
     'mongoquery',
-    'convex-api-py >= 0.1.2',
+    'convex-api-py >= 0.1.3',
 ]
 
 setup_requirements = ['pytest-runner', ]
